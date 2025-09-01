0bade5c505902dc61e59fab4ed2419278294d827
vicentecarlos <vicentecarloshehe@gmail.com>
2025-09-01 06:08:11 -0300

    fix: fallback auth

--- network/actions/auth/socialAuth.ts
+++ network/actions/auth/socialAuth.ts
@@ -1,11 +1,11 @@
 export const initiateGoogleAuth = async () => {
   // Redireciona para o endpoint de login do Google no backend
-  window.location.href = `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001'}/google/login`
+  window.location.href = `${process.env.NEXT_PUBLIC_API_URL || 'https://ichef24-api-3.onrender.com'}/google/login`
 }
 
 export const initiateFacebookAuth = async () => {
   // Redireciona para o endpoint de login do Facebook no backend
-  window.location.href = `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001'}/facebook/login`
+  window.location.href = `${process.env.NEXT_PUBLIC_API_URL || 'https://ichef24-api-3.onrender.com'}/facebook/login`
 }
 
  
